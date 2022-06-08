b63399ea8e23b75a01fbeef69005d9f53d50c2ef
Vladimir Loginov <BoJIodya@yandex.ru>
2022-06-08 21:03:05 +0300

    доделал решение, украсил вывод результата

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ void PrintArray(string[] Array)            // метод печати масси
 {
     for (int i = 0; i < Array.Length; i++)
     {
-        Console.Write($" {Array[i]};");
+        Console.Write($" {Array[i]}");
     }
 }
 
@@ -24,9 +24,10 @@ string[] CountSymbol(string[] array) // метод вычисления длин
 }
 
 string[] ArrayString = new string[7] { "Hello", "123", "World", "kat", "85", "-9", "dog" };
-string[] ArrayResult = new string[ArrayString.Length];
-
+string[] ArrayResult = new string[ArrayString.Length];  //длину массива резервируем равную исходному массиву,
+                                                        //поскольку могут быть все значения <=3
 PrintArray(ArrayString);
 ArrayResult = CountSymbol(ArrayString);
 Console.WriteLine();
+Console.WriteLine($" Резельтат вычислений:");
 PrintArray(ArrayResult);
