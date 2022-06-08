8b85f7f5e17144dc172ba183770a32062e299434
Vladimir Loginov <BoJIodya@yandex.ru>
2022-06-08 20:22:14 +0300

    add void PrintArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void PrintArray (string[] Array)            // метод печати массива
+{
+    for (int i = 0; i< Array.Length; i++)
+    {
+        Console.Write($" {Array[i]};");
+    }
+}
+
+
+
+string[] ArrayString = new string[7] {"Hello", "123", "World", "kat", "85", "-9", "dog"};
+string[] ArrayResult = new string[ArrayString.Length];
+PrintArray(ArrayString);
